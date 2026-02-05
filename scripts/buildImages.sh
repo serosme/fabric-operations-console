e3e1d749d52f13eaa6f80ac069a0416849f788e5
seros <github@seros.me>
2026-02-05 16:08:57 +0800

    修复 buildImages.sh 中 COMPONENT 变量的赋值逻辑，确保参数默认值正确
    Signed-off-by: seros <github@seros.me>

--- scripts/buildImages.sh
+++ scripts/buildImages.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 PLATFORM=${PLATFORM:-linux/amd64}
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # Where the script lives
 
-COMPONENT=$1
+COMPONENT=${1:-}
 if [ "${COMPONENT}" == "grpc-web" ]; then
 	# build grpc proxy image
 	docker buildx build --platform "${PLATFORM}" --load -t ghcr.io/hyperledger-labs/grpc-web:latest -f ${SRC_DIR}/../docker/grpc-web/Dockerfile .
